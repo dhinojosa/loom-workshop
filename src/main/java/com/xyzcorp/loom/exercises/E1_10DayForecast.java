929935d987afcf57e989a6fe8ae63a8032e401d3
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2021-04-08 12:34:57 -0600

    Fixed the Exercises

--- src/main/java/com/xyzcorp/loom/exercises/E1_10DayForecast.java
+++ src/main/java/com/xyzcorp/loom/exercises/E1_10DayForecast.java
@@ -1,37 +1,60 @@
 package com.xyzcorp.loom.exercises;
 
-import com.xyzcorp.loom.demo.D7_StructuredConcurrencyNested;
-
+import java.io.PrintStream;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Random;
-import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadFactory;
+import java.util.stream.Stream;
 
 public class E1_10DayForecast {
 
     public static String getRandomWeatherString() {
         Random random = new Random();
-        List<String> conditions = List.of("Sunny", "Overcast", "Raining", "Snowing", "Hail");
+        List<String> conditions = List.of("Sunny", "Overcast", "Raining",
+            "Snowing", "Hail");
         return conditions.get(random.nextInt(conditions.size()));
     }
 
-    public static void getRandomWeatherByDate(LocalDate localDate){
+    public static void getRandomWeatherByDate(LocalDate localDate) {
         ThreadFactory tf = Thread.builder().virtual().factory();
         try (ExecutorService e = Executors.newThreadExecutor(tf)) {
-            //iterate over 12AM - 11PM
+            Stream.iterate(localDate.atStartOfDay(),
+                localDateTime -> isSameDay(localDate, localDateTime),
+                ldt -> ldt.plusHours(1))
+                  .forEach(localDateTime ->
+                      e.submit(() ->
+                          printWeatherFor(localDate, localDateTime)));
         }//join
     }
-    //Create one Method that will use an execution context to iterate for the next 10 days
+
+    private static PrintStream printWeatherFor(LocalDate localDate,
+                                         java.time.LocalDateTime localDateTime) {
+        return System.out.printf("%s %s %s\n", localDate, localDateTime,
+            getRandomWeatherString());
+    }
+
+    private static boolean isSameDay(LocalDate localDate,
+                                      java.time.LocalDateTime localDateTime) {
+        return localDate.getDayOfMonth() == localDateTime.getDayOfMonth() &&
+            localDate.getMonth() == localDateTime.getMonth();
+    }
+
+    //Create one Method that will use an execution context to iterate for the
+    // next 10 days
     public static void iterateOverNextTenDays() {
         ThreadFactory tf = Thread.builder().virtual().factory();
         try (ExecutorService e = Executors.newThreadExecutor(tf)) {
-            //iterate over the next 10 days
+            Stream
+                .iterate(LocalDate.now(), localDate -> localDate.plusDays(1))
+                .limit(10)
+                .forEach(date -> e.submit(() -> getRandomWeatherByDate(date)));
         }//join
     }
+
     public static void main(String[] args) {
-       iterateOverNextTenDays();
+        iterateOverNextTenDays();
     }
 }
